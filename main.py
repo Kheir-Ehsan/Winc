5066207896aa193f066589d11bc0cb89c0e79549
Kheir-Ehsan <110845500+Kheir-Ehsan@users.noreply.github.com>
2022-09-20 17:08:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,25 @@ goal_1 = 54
 # str () - converts to string
 # int () - converts to integer
 # eg. print (scorer_name1 +' scored the first goal in ' + str (goal_0) + " minutes")
-#scorers = scorer_name1 + ' ' + str (goal_0) + ", " + scorer_name2 + ' ' + str (goal_1)
-#report = scorer_name1 + ' scored in the ' + str(goal_0) +'nd' + ' minute' + ' ' + scorer_name2 + ' scored in the ' + str(goal_1) +'th' + ' minute'
+#scorers = scorer_name1 + ' ' + str(goal_0) + ", " + scorer_name2 + ' ' + str(goal_1)
 scorers = f'{scorer_name1} {goal_0}, {scorer_name2} {goal_1}'
-report = f'{scorer_name1} sccored in {goal_0}nd minute {scorer_name2} scored in the {goal_1}th minute'
+report = f'{scorer_name1} scored in the {goal_0}nd minute\n{scorer_name2} scored in the {goal_1}th minute'
+#report = scorer_name1 + ' scored in the ' + str(goal_0)+'nd' + ' minute' + ' ' + scorer_name2 + \
+' scored in the ' + str(goal_1)+'th' + ' minute'
 print(report)
 
-last_name = 'Koeman' 
-player = ['Ronald', 'Koeman']
-first_name = player[0] # slice and store first name. Here we isolate letter 0 to 6 tot get the first name
-last_name_len = len(last_name) # Here we store the length of the var. last_name
+player= 'Ronald Koeman'
+x = player.find(' ')
+first_name = player[:x]
+#first_name = player[:player.find(" ")]
+print(first_name)
+last_name = player[-x:]
+#last_name = player[player.find(" "):]
+print (last_name)
+last_name_len = len(player[-x:])
+print (last_name_len)
 name_short = first_name[0] +". " + last_name
-chant = first_name + "!" + ' '
-print (chant * len(first_name))
+chant = len(first_name) * (first_name + "! ")
+chant = chant.rstrip ( )
+good_chant = chant[-1] != " "
+print (chant)

c7847e6408265ed6a2996946d0b5eb125c33c89f
Kheir-Ehsan <110845500+Kheir-Ehsan@users.noreply.github.com>
2022-10-10 16:35:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,30 @@ __human_name__ = 'strings'
 
 # Add your code after this line
 
-from gettext import find
-
-
 scorer_name1 = 'Ruud Gullit'
 scorer_name2 = 'Marco van Basten'
+
 goal_0 = 32
 goal_1 = 54
-# str () - converts to string
-# int () - converts to integer
-# eg. print (scorer_name1 +' scored the first goal in ' + str (goal_0) + " minutes")
-#scorers = scorer_name1 + ' ' + str(goal_0) + ", " + scorer_name2 + ' ' + str(goal_1)
+
 scorers = f'{scorer_name1} {goal_0}, {scorer_name2} {goal_1}'
 report = f'{scorer_name1} scored in the {goal_0}nd minute\n{scorer_name2} scored in the {goal_1}th minute'
-#report = scorer_name1 + ' scored in the ' + str(goal_0)+'nd' + ' minute' + ' ' + scorer_name2 + \
-' scored in the ' + str(goal_1)+'th' + ' minute'
 print(report)
 
-player= 'Ronald Koeman'
-x = player.find(' ')
-first_name = player[:x]
-#first_name = player[:player.find(" ")]
+
+player= 'Kheirullah Ehsan'
+first_name = player[:player.find(' ')]
 print(first_name)
-last_name = player[-x:]
-#last_name = player[player.find(" "):]
+
+last_name = player[player.find(' ')+1:] #without the +1 it also gets the space
 print (last_name)
-last_name_len = len(player[-x:])
+
+last_name_len = len(last_name)
 print (last_name_len)
+
 name_short = first_name[0] +". " + last_name
+print (name_short)
+
 chant = len(first_name) * (first_name + "! ")
 chant = chant.rstrip ( )
 good_chant = chant[-1] != " "
